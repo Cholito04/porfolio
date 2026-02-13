2716a1292e6e46f97d5e7b8bd463e5001225cada
Cholito04 <mauriciomancerab@gmail.com>
2026-02-12 19:46:57 -0500

    changed title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,15 @@ function Hero() {
   return (
     <div
       id="Hero"
-      className="w-full lg:px-4 px-10 py-40 mx-auto text-white text-center"
+      className="w-full lg:px-4 px-10 py-20 mx-auto text-white text-center"
     >
       <div className="mx-auto lg:max-w-310 max-w-200 bg-neutral-800 p-20 rounded-4xl shadow-2xl">
-        <h1 className=" lg:text-6xl md:text-5xl text-2xl font-extrabold tracking-tight">
-          Mauricio Mancera-Bohorquez
+        <h1 className="lg:text-6xl md:text-5xl text-3xl font-extrabold tracking-tight">
+          Mauricio
+          <br />
+          <span className="bg-linear-to-b from-sky-200 to-sky-800 bg-clip-text text-transparent">
+            Mancera-Bohorquez
+          </span>
         </h1>
         <p className="md:text-xl sm:text-l py-4">
           CS @ Western Michigan University | I build clean, interactive web
